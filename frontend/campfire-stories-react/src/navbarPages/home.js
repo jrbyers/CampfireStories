01bea4dcad3e25e88da366bfb74c2330a28afd7a
natcasd <nathan_depiero@brown.edu>
2023-01-29 10:35:43 -0500

    Update home.js

--- frontend/campfire-stories-react/src/navbarPages/home.js
+++ frontend/campfire-stories-react/src/navbarPages/home.js
@@ -81,8 +81,7 @@ const InsideText = styled.div`
 function GenerateStory(prompt, setStory,loadbool,setLoadBool) {
     setStory("")
     const apiKeyNate = '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';
-    const apiKeyJR = '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'
-    const apiKeyMarcus = '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';
+    
 
     const tips = ["camping tips", "campfire safety", "bear safety", "smokey the bear", "tips on setting up a tent", "tips on how to pick a campsite", "campfire safety"]
     const randomIndex = Math.floor(Math.random() * tips.length);
@@ -91,7 +90,7 @@ function GenerateStory(prompt, setStory,loadbool,setLoadBool) {
     const promptfull = 'write a spooky story with ' + prompt + ' with ' + randomElement;
 
     const headers = {
-        'Authorization': `Bearer ${apiKeyMarcus}`
+        'Authorization': `Bearer ${apiKeyNate}`
     }
 
     setLoadBool(!loadbool)
