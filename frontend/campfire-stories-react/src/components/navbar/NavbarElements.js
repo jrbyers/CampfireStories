e7f4d0c6595b743c4471e9c68ae4d1878bdee27f
jrbyers <john_byers@brown.edu>
2023-01-28 19:40:05 -0500

    navbar shrink

--- frontend/campfire-stories-react/src/components/navbar/NavbarElements.js
+++ frontend/campfire-stories-react/src/components/navbar/NavbarElements.js
@@ -6,7 +6,7 @@ import styled from 'styled-components';
 
 export const Nav = styled.nav`
 background: #dda0dd;
-height: 85px;
+height: 50px;
 display: flex;
 justify-content: space-between;
 padding: 0.2rem calc((100vw - 1000px) / 2);
